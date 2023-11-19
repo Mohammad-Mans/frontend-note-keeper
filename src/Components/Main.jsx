84479e9e988771a9028479d0b37a35ce8f088684
Mohammad Mansour <mohammed.i02@hotmail.com>
2023-11-19 19:53:52 +0200

    update api

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import "./Main.css";
 import AddNotesField from "./AddNotesField";
 import Notes from "./Notes";
-import { fetchNotes, addNote, deleteNote, updateNote } from './CRUDNoteAPI';
 
 function Main({ filter }) {
   const [notes, setNotes] = useState([]);
   const [filteredNotes, setFilteredNotes] = useState(null);
+  const api = '/api';
 
   useEffect(() => {
     fetchData();
@@ -26,23 +26,82 @@ function Main({ filter }) {
   }, [filter, notes]);
 
   const fetchData = async () => {
-    const data = await fetchNotes();
-    setNotes(data);
+    try {
+      const response = await fetch(api);
+      if (response.ok) {
+        const data = await response.json();
+        setNotes(data);
+      } else {
+        console.error("Failed to fetch notes");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   const handleAddNote = async (note) => {
-    await addNote(note);
-    fetchData();
-  };
+    try {
+      const response = await fetch(api, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: note.title,
+          content: note.content,
+        }),
+      });
+
+      if (response.ok) {
+        console.log("Note added successfully");
+        fetchData();
+      } else {
+        console.error("Failed to add note");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   const handleDeleteNote = async (noteId) => {
-    await deleteNote(noteId);
-    fetchData();
+    try {
+      const response = await fetch(`${api}/${noteId}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        console.log("Note deleted successfully");
+        fetchData();
+      } else {
+        console.error("Failed to delete note");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   const handleUpdateNote = async (noteId, updatedNote) => {
-    await updateNote(noteId, updatedNote);
-    fetchData();
+    try {
+      const response = await fetch(`${api}/${noteId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: updatedNote.title,
+          content: updatedNote.content,
+        }),
+      });
+
+      if (response.ok) {
+        console.log("Note updated successfully");
+        fetchData();
+      } else {
+        console.error("Failed to update note");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
